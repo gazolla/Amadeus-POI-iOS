9883e8a77621229a67a9188d2a6b8d005028b4fa
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-08-17 13:27:49 -0300

    Initial Commit

---
+++ Amadeus-POI-iOS/Amadeus_POI_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amadeus_POI_iOSApp.swift
+//  Amadeus-POI-iOS
+//
+//  Created by sebastiao Gazolla Costa Junior on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Amadeus_POI_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
