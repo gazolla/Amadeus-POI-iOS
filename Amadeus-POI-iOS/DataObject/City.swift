a74b9db730d8dceb2f4fb936c15f1ec0979ec6a5
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-08-17 16:36:34 -0300

    Add City Data Object

---
+++ Amadeus-POI-iOS/DataObject/City.swift
@@ -0,0 +1,29 @@
+//
+//  City.swift
+//  Amadeus-POI-iOS
+//
+//  Created by sebastiao Gazolla Costa Junior on 17/08/22.
+//
+
+import Foundation
+import MapKit
+
+
+struct City: Identifiable, Equatable{
+    var id = UUID()
+    var name:String?
+    var country:String?
+    var flagIconURL: String?
+    var placemark:CLPlacemark?
+    
+    init(placemark:CLPlacemark){
+        self.name = placemark.name
+        self.country = placemark.country
+        self.flagIconURL = placemark.isoCountryCode
+        self.placemark = placemark
+    }
+    
+    static func == (lhs: City, rhs: City) -> Bool {
+        lhs.id == rhs.id
+    }
+}
