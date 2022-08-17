25b66cd207f179b8440599c1ce2896efb6f48c3c
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-08-17 17:54:49 -0300

    Add CityEmptyView

---
+++ Amadeus-POI-iOS/View/CityEmptyView.swift
@@ -0,0 +1,33 @@
+//
+//  CityEmptyView.swift
+//  Amadeus-POI-iOS
+//
+//  Created by sebastiao Gazolla Costa Junior on 17/08/22.
+//
+
+import SwiftUI
+
+struct CityEmptyView: View {
+    var body: some View {
+        ScrollView{
+            VStack(spacing: 10) {
+                
+                Image("City")
+                    .padding()
+                Text ("There is no City!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("You should click the add button and add one!")
+                    .multilineTextAlignment(.center)
+                    .padding(40)
+            }
+        }
+        .frame(maxWidth:.infinity, maxHeight:.infinity)
+    }
+}
+
+struct CityEmptyView_Previews: PreviewProvider {
+    static var previews: some View {
+        CityEmptyView()
+    }
+}
